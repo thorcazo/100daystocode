026df40d42aacd312bd7579b4d6a73839d2fd08c
thorcazo <thorcazodesign@gmail.com>
2024-07-29 21:02:51 +0200

    cambios de estilos CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
 import './App.css'
 
+
+/* IMPORT ICONS */
+import Linkedin from './components/icons/Linkedin';
+import Facebook from './components/icons/Facebook';
+import Twitter from './components/icons/Twitter';
+
+
 function App() {
 
   return (
     <>
-      <section>
-        <article className='card'>
-          <div>
-            <h3>Jonny Rogers</h3>
-            <span>@jonnyrogers</span>
-          </div>
+      <article className='card'>
+        <img className='card__image' src="./assets/foto.jpg" alt="" />
+        <div className='card__name'>
+          <h3 className='card__name-title'>Jonny Rogers</h3>
+          <span className='card__name-nick'>@jonnyrogers</span>
+        </div>
 
-          {/* ICONO REDES SOCIALES */}
-          <ul>
-            <li>Facebook</li>
-            <li>Linkedin</li>
-            <li>Twitter</li>
-            <li>Instagram</li>
-            <li>Dribbble</li>
-          </ul>
+        {/* ICONO REDES SOCIALES */}
+        <ul className='card__social'>
+          <a href='#' className='card__social-item'><Facebook /></a>
+          <a href='#' className='card__social-item'><Linkedin /></a>
+          <a href='#' className='card__social-item'><Twitter /></a>
+        </ul>
 
-          <p>Crafting brand and communication stratigies, creating visual designs, leading art direction, and capturing portraits through photography</p>
+        <p>Crafting brand and communication stratigies, creating visual designs, leading art direction, and capturing portraits through photography</p>
 
-          <div>
-            <button>Follow</button>
-            <button>Message</button>
-          </div>
-        </article>
+        <div className=''>
+          <button className='card__button pink'>Follow</button>
+          <button className='card__button'>Message</button>
+        </div>
+      </article>
 
-      </section>
     </>
   )
 }
