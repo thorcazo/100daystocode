ab0501f19fd4d27189bccd5402b821bd2fed1537
thorcazo <thorcazodesign@gmail.com>
2024-07-29 20:15:22 +0200

    Creado el HTML necesario

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,31 @@ function App() {
 
   return (
     <>
-     
+      <section>
+        <article className='card'>
+          <div>
+            <h3>Jonny Rogers</h3>
+            <span>@jonnyrogers</span>
+          </div>
+
+          {/* ICONO REDES SOCIALES */}
+          <ul>
+            <li>Facebook</li>
+            <li>Linkedin</li>
+            <li>Twitter</li>
+            <li>Instagram</li>
+            <li>Dribbble</li>
+          </ul>
+
+          <p>Crafting brand and communication stratigies, creating visual designs, leading art direction, and capturing portraits through photography</p>
+
+          <div>
+            <button>Follow</button>
+            <button>Message</button>
+          </div>
+        </article>
+
+      </section>
     </>
   )
 }
