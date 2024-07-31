3b6a047a6445786a6253d752aceae2da92040a9f
thorcazo <thorcazodesign@gmail.com>
2024-07-31 17:09:41 +0200

    Terminada la animación de despliegue de los items del menú

--- src/components/MainMenuItem.jsx
+++ src/components/MainMenuItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 /* IMPORTAR LOS ICONOS */
 import UserIcon from './icons/UserIcon'
@@ -19,23 +19,38 @@ const icons = {
 };
 
 
+
+
+
 export default function MainMenuItem({ icon, text }) {
   const IconComponent = icons[icon] || null;
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = (icon) => {
+    if (icon) {
+      setIsOpen(!isOpen);
+      console.log(isOpen);
+    }
+  }
 
   return (
     <>
-      <div className='main-menu__item'>
+      <div className='main-menu__item'
+        onClick={() => handleClick(icon)}
+      >
         <div>
-          {IconComponent}
-          <p>{text}</p>
+          <div className='item__content'>
+            {IconComponent}
+            <p>{text}</p>
+          </div>
+          <Arrow className="main-menu__arrow" />
         </div>
-        <Arrow className="main-menu__arrow" />
-      </div>
-      <div>
-        <p>
-          informacion relevante
-        </p>
+        <div className={ isOpen ? 'content__text active-content' : 'content__text'}>
+          <p>
+            hola
+          </p>
+        </div>
       </div>
     </>
   )
